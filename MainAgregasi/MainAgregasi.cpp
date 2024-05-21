bbcc4409a3a2ac9dd63e3455a93276b962ef7024
ZakyMalika <zakymalika0987@gmail.com>
2024-05-21 19:34:53 +0700

    membuat varIbu

--- MainAgregasi/MainAgregasi.cpp
+++ MainAgregasi/MainAgregasi.cpp
@@ -7,5 +7,18 @@ using namespace std;
 
 int main()
 {
+	ibu* varIbu = new ibu("nabil");
+	ibu* varibu2 = new ibu("nabil");
+	anak* varAnak1 = new anak("nabil");
+	anak* varAnak2 = new anak("nabil");
+	anak* varAnak3 = new anak("nabil");
+
+	varIbu->tambahAnak(varAnak1);
+	varIbu->tambahAnak(varAnak1);
+	varibu2->tambahAnak(varAnak1);
+	varibu2->tambahAnak(varAnak1);
+	
+
+
 
 }
