06ca7b1afe5b19f9fc99fdbaf85feb532d869662
ZakyMalika <zakymalika0987@gmail.com>
2024-05-21 18:18:57 +0700

    menggunakan acces modifier public

--- mainiJM/Jantung.h
+++ mainiJM/Jantung.h
@@ -4,6 +4,15 @@ using namespace std;
 
 class jantung {
 public:
+	jantung()
+	{
+		cout << "jantung dihidupkan\n";
+
+	}
+	~jantung()
+	{
+		cout << "jantung dimatikan\n";
+	}
 };
 
 #endif // !JANTUNG_H
